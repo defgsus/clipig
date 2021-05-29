78912aab21a0ebb880615b911561535ffa4ffbdd
bergi <s.berke@netzkolchose.de>
2021-05-29 10:29:55 +0200

    add feature caching to rate.py

--- rate.py
+++ rate.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import hashlib
 import argparse
 import pathlib
 from typing import List, Optional, Tuple, Union
@@ -6,6 +8,7 @@ from typing import List, Optional, Tuple, Union
 import pandas as pd
 import torch
 import clip
+import PIL
 from tqdm import tqdm
 
 from src.clip_singleton import ClipSingleton
@@ -27,6 +30,14 @@ def parse_arguments():
         help="Reference images to which images are compared",
     )
     parser.add_argument(
+        "-tf", "--text-file", type=str, default=None,
+        help="File with text prompts on each line to which images are compared",
+    )
+    parser.add_argument(
+        "-if", "--image-file", type=str, default=None,
+        help="File with reference image filenames on each line to which images are compared",
+    )
+    parser.add_argument(
         "-d", "--device", type=str, default="auto",
         help="Device to run on, either 'auto', 'cuda' or 'cuda:1', etc... "
              "Default is 'auto'",
@@ -43,10 +54,14 @@ def parse_arguments():
         "-o", "--output", type=str, default=None,
         help=f"Filename of a CSV file to store the output",
     )
+    parser.add_argument(
+        "-c", "--cache", type=bool, default=False, const=True, nargs="?",
+        help=f"Enable caching of image features",
+    )
 
     args = parser.parse_args()
 
-    if not args.text and not args.image:
+    if not args.text and not args.image and not args.text_file and not args.image_file:
         print("Need to define at least one '-t/--text' or '-i/--image'")
         exit(1)
 
@@ -54,6 +69,8 @@ def parse_arguments():
 
 
 class ClipRater:
+
+    CACHE_DIR = pathlib.Path("~").expanduser() / ".cache" / "clipig-rate"
 
     def __init__(
             self,
@@ -63,21 +80,23 @@ class ClipRater:
             model: str = "ViT-B/32",
             device: str = "auto",
             batch_size: int = 10,
+            chaching: bool = False,
     ):
         self.filenames = filenames
         self.texts = texts
         self.images = images
         self.batch_size = batch_size
+        self.caching = chaching
         self.model, self.preprocess = ClipSingleton.get(model, device)
         self.device = self.model.logit_scale.device
 
     def rate(self) -> pd.DataFrame:
-        image_features = self._get_image_file_features(self.filenames, "encoding images")
+        image_features, filenames = self._get_image_file_features(self.filenames, "encoding images")
         compare_features, columns = self._get_all_compare_features()
 
         similarities = 100. * image_features @ compare_features.T
 
-        df = pd.DataFrame(similarities.numpy(), index=self.filenames, columns=columns)
+        df = pd.DataFrame(similarities.numpy(), index=filenames, columns=columns)
         return df
 
     def _get_image_features(self, images: List) -> torch.Tensor:
@@ -98,36 +117,87 @@ class ClipRater:
             text_features = text_features / text_features.norm(dim=-1, keepdim=True)
             return text_features.cpu()
 
-    def _get_image_file_features(self, filenames: List[str], desc: str = None) -> torch.Tensor:
-        image_features = []
+    def _get_image_file_features(
+            self,
+            filenames: List[str],
+            desc: str = None
+    ) -> Tuple[torch.Tensor, List[str]]:
+        image_features = dict()
         images = []
         for filename in tqdm(filenames, desc=desc):
-            image = load_image(filename)
-            image = resize_crop(image, [224, 224])
-            image = self.preprocess(image)
-            images.append(image.unsqueeze(0))
+            if self.caching:
+                feature = self._get_cache(filename)
+                if feature is not None:
+                    image_features[filename] = feature
+                    continue
+
+            try:
+                image = load_image(filename)
+                image = resize_crop(image, [224, 224])
+                image = self.preprocess(image)
+            except (PIL.UnidentifiedImageError, OSError):
+                print(f"{filename} seems to be no image", file=sys.stderr)
+                continue
+
+            images.append((filename, image))
 
             if len(images) >= self.batch_size:
-                image_features.append(self._get_image_features(images))
+                features = self._get_image_features([i[1].unsqueeze(0) for i in images])
+                for (filename, _), feature in zip(images, features):
+                    feature = feature
+                    image_features[filename] = feature
+                    if self.caching:
+                        self._save_cache(filename, feature)
                 images = []
 
         if images:
-            image_features.append(self._get_image_features(images))
+            features = self._get_image_features([i[1].unsqueeze(0) for i in images])
+            for (filename, _), feature in zip(images, features):
+                image_features[filename] = feature
+                if self.caching:
+                    self._save_cache(filename, feature)
+
+        filenames = list(filter(lambda f: f in image_features, filenames))
+
+        image_features = [
+            image_features[fn].unsqueeze(0)
+            for fn in filenames
+        ]
 
         image_features = torch.cat(image_features)
-        return image_features
+        return image_features, filenames
 
     def _get_all_compare_features(self) -> Tuple[torch.Tensor, List[str]]:
         compare_features = []
+        compare_feature_names = []
         texts = self.texts
         while texts:
             compare_features.append(self._get_text_features(texts[:self.batch_size]))
+            compare_feature_names += texts[:self.batch_size]
             texts = texts[self.batch_size:]
 
         if self.images:
-            compare_features.append(self._get_image_file_features(self.images, "encoding compare images"))
+            features, filenames = self._get_image_file_features(self.images, "encoding compare images")
+            compare_features.append(features)
+            compare_feature_names += filenames
 
-        return torch.cat(compare_features), (self.texts or []) + (self.images or [])
+        return torch.cat(compare_features), compare_feature_names
+
+    def _get_cache_filename(self, filename: str) -> pathlib.Path:
+        path = pathlib.Path(filename).resolve()
+        key = f"{path}-{path.stat().st_mtime}"
+        return self.CACHE_DIR / hashlib.md5(key.encode("utf-8")).hexdigest()
+
+    def _get_cache(self, filename: str) -> Optional[torch.Tensor]:
+        fn = self._get_cache_filename(filename)
+        if fn.exists():
+            return torch.load(fn)
+
+    def _save_cache(self, filename: str, tensor: torch.Tensor):
+        fn = self._get_cache_filename(filename)
+        if not self.CACHE_DIR.exists():
+            os.makedirs(self.CACHE_DIR)
+        torch.save(tensor, str(fn))
 
 
 def trim_length(s: str, max_len: int, front: Union[bool, str] = False) -> str:
@@ -156,17 +226,32 @@ def dump_similarities(similarities: pd.DataFrame):
     # similarities = similarities.round(2)
     print(similarities)
 
+    print(similarities.describe())
 
-if __name__ == "__main__":
 
+def main():
     args = parse_arguments()
+
+    texts = args.text or []
+    images = args.image or []
+
+    if args.text_file:
+        texts = texts + list(
+            filter(bool, pathlib.Path(args.text_file).read_text().splitlines())
+        )
+
+    if args.image_file:
+        images = images + list(
+            filter(bool, pathlib.Path(args.image_file).read_text().splitlines())
+        )
 
     rater = ClipRater(
         filenames=args.file,
-        texts=args.text,
-        images=args.image,
+        texts=texts,
+        images=images,
         model=args.model,
         device=args.device,
+        chaching=args.cache,
     )
     similarities = rater.rate()
 
@@ -185,3 +270,7 @@ if __name__ == "__main__":
 
     if args.output:
         similarities.to_csv(args.output)
+
+
+if __name__ == "__main__":
+    main()
